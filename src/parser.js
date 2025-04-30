5a22234cb2370517cf892a102d0ad56f7ecd8f0a
pr4j3sh <prajesh.eleven118@gmail.com>
2025-05-01 01:20:12 +0530

    feat: added ignore list as separate file

--- src/parser.js
+++ src/parser.js
@@ -5,7 +5,7 @@ class Parser {
   constructor(dirPath) {
     if (!dirPath) throw new Error("directory path cannot be empty");
     this.dirPath = dirPath;
-    this.ignore = ["node_modules", "package-lock.json", "dist", "venv"];
+    this.ignore = [];
     this.tree = [];
   }
 
